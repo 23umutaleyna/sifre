36a9723d200074a64e894f5f7ff0992909fecc32
23umutaleyna <145720061+23umutaleyna@users.noreply.github.com>
2026-09-25 21:22:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-fsdfsdfsdfsdf
+import random
+
+karakterler="+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+sifre=""
+
+sifre_uzunlugu=int(input("şifreniz kaç haneli olsun?"))
+
+for i in range(sifre_uzunlugu):
+    secilen_karakter=random.choice(karakterler)
+    sifre+=secilen_karakter
+
+print("şifreniz:",sifre)   
